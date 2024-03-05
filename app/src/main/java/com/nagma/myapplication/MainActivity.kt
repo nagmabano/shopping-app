b123acd18a425029207259094ed79ef0d99c45bf
Nagma Bano <bano.nagma.1@gmail.com>
2024-03-05 21:48:09 +0530

    binding view to data

--- app/src/main/java/com/nagma/myapplication/MainActivity.kt
+++ app/src/main/java/com/nagma/myapplication/MainActivity.kt
@@ -2,14 +2,46 @@ package com.nagma.myapplication
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.google.android.material.snackbar.Snackbar
 import com.nagma.myapplication.databinding.ActivityMainBinding
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val viewModel by viewModels<MainViewModel>()
+
+        binding.addButton.setOnClickListener{ view ->
+            viewModel.increaseItemCount()
+        }
+
+        binding.minusButton.setOnClickListener{ view ->
+            viewModel.decreaseItemCount()
+        }
+
+        binding.checkoutButton.setOnClickListener{view ->
+            var result = viewModel.getPrice()
+            displaySnackbar(result)
+        }
+
+        viewModel.itemQuantity.observe(this) {
+            binding.countNumber.setText(viewModel.itemQuantity.value.toString())
+        }
+
+        viewModel.itemPrice.observe(this) {
+            binding.itemPrice.setText("Rs." + viewModel.itemPrice.value.toString())
+        }
+
+    }
+
+    private fun displaySnackbar(count: Int) {
+        Snackbar.make(binding.root, "Total Price: $count", Snackbar.LENGTH_LONG).show()
     }
 }
