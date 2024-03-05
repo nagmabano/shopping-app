3f89dc2214391fcd06326edb7f10fac8605c12a5
Nagma Bano <bano.nagma.1@gmail.com>
2024-03-05 21:47:41 +0530

    to import viewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,8 @@ dependencies {
 
     implementation("com.google.android.material:material:1.11.0")
 
+    implementation("androidx.activity:activity-ktx:1.8.2")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
