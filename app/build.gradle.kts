1c77276e622b10594887e4e2098abc21d2e859b8
Nagma Bano <bano.nagma.1@gmail.com>
2024-03-08 13:28:33 +0530

    added a fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
 
     implementation("androidx.activity:activity-ktx:1.8.2")
 
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
--- app/src/main/java/com/nagma/myapplication/MainActivity.kt
+++ app/src/main/java/com/nagma/myapplication/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.nagma.myapplication
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import androidx.fragment.app.add
 import androidx.lifecycle.Observer
 import com.google.android.material.snackbar.Snackbar
 import com.nagma.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -37,6 +41,10 @@ class MainActivity : AppCompatActivity() {
 
         viewModel.itemPrice.observe(this) {
             binding.itemPrice.setText("Rs." + viewModel.itemPrice.value.toString())
+        }
+
+        supportFragmentManager.commit{
+            add<ProductFragment>(R.id.container, null)
         }
 
     }
