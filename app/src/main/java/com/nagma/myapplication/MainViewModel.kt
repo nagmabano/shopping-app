5a58f889cde5996800fd2862db570e4fa2d774e2
Nagma Bano <bano.nagma.1@gmail.com>
2024-03-08 14:00:54 +0530

    read files from resources

--- app/src/main/java/com/nagma/myapplication/MainViewModel.kt
+++ app/src/main/java/com/nagma/myapplication/MainViewModel.kt
@@ -1,15 +1,28 @@
 package com.nagma.myapplication
 
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.nagma.myapplication.data.ProductRepository
 
-class MainViewModel(): ViewModel() {
+class MainViewModel(app:Application): AndroidViewModel(app) {
 
     private val _itemQuantity: MutableLiveData<Int> = MutableLiveData()
     val itemQuantity:LiveData<Int> = _itemQuantity
     private val _itemPrice: MutableLiveData<Int> = MutableLiveData()
     val itemPrice:LiveData<Int> = _itemPrice
+
+    var productRepository: ProductRepository = ProductRepository()
+
+    init {
+        val data = productRepository.getTextFromResource(app, R.raw.product)
+
+        Log.i("Data","data: $data")
+    }
+
     init {
         _itemQuantity.value = 0
         _itemPrice.value = 50
---
+++ app/src/main/java/com/nagma/myapplication/data/ProductRepository.kt
@@ -0,0 +1,12 @@
+package com.nagma.myapplication.data
+
+import android.content.Context
+
+class ProductRepository {
+
+    fun getTextFromResource(context: Context, resourceId: Int): String {
+        return context.resources.openRawResource(resourceId)
+            .bufferedReader()
+            .use { it.readText() }
+    }
+}
