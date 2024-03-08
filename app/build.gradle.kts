42fa539d933365fb09253db456a57ef6eb8a79be
Nagma Bano <bano.nagma.1@gmail.com>
2024-03-08 18:10:53 +0530

    added retrofit dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,10 @@ dependencies {
     implementation("com.squareup.moshi:moshi-kotlin:1.11.0")
 
     implementation("androidx.core:core-ktx:1.9.0")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
