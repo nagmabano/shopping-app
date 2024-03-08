d87656a663c8893533b70b7a31f7edf89ef65e0d
Nagma Bano <bano.nagma.1@gmail.com>
2024-03-08 13:29:19 +0530

    model an entity in kotlin data class

---
+++ app/src/main/java/com/nagma/myapplication/data/Product.kt
@@ -0,0 +1,9 @@
+package com.nagma.myapplication.data
+
+data class Product(
+    val name: String,
+    val imageFile:String,
+    val description:String,
+    val size: Int,
+    val price: Double
+)
